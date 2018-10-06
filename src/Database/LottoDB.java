eee7340a07994e0c3b9c1796e63acdac4586222c
luigirivera <luigirivera@users.noreply.github.com>
2018-10-06 09:34:46 +0800

    DB jar

---
+++ src/Database/LottoDB.java
@@ -0,0 +1,9 @@
+package Database;
+
+public class LottoDB {
+	private static String DRIVER_NAME = "com.mysql.jdbc.Driver";
+	private static String URL = "jdbc:mysql://127.0.0.1:3306/";
+	private static String USERNAME = "root";
+	private static String PASSWORD = "agentblue";
+	private static String DATABASE = "UDC";
+}
